9efd8a9025e88c21eacbef40b656a47219e74b4c
LizHackSA <sbuda.ncanywa@gmail.com>
2019-12-25 12:39:26 +0200

    finished adding the services pages

--- version3/WebRoot/Assets/js/script.js
+++ version3/WebRoot/Assets/js/script.js
@@ -1,33 +1,33 @@
 const menuBtn = document.querySelector('.menu-btn');
 const hamBurger = document.querySelector('.menu-btn__burger');
 
-const nav = document.querySelector('.nav');
-const menuNav = document.querySelector('.menu-nav');
+//const nav = document.querySelector('.nav');
+//const menuNav = document.querySelector('.menu-nav');
 
-const navitems = document.querySelectorAll('.menu-nav__item');
-
+//const navitems = document.querySelectorAll('.menu-nav__item');
 
 let showMenu = false;
 
+// add event listener
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     if (showMenu) {
         // if menu is open close it
         hamBurger.classList.remove('open');
-        nav.classList.remove('open');
-        menuNav.classList.remove('open');
+        //nav.classList.remove('open');
+        //menuNav.classList.remove('open');
 
-        navitems.forEach(item => item.classList.remove('open'));
+        //navitems.forEach(item => item.classList.remove('open'));
 
         showMenu = false;
     } else {
         // if menu is closed open it
         hamBurger.classList.add('open');
-        nav.classList.add('open');
-        menuNav.classList.add('open');
+        //nav.classList.add('open');
+        //menuNav.classList.add('open');
 
-        navitems.forEach(item => item.classList.add('open'));
+        //navitems.forEach(item => item.classList.add('open'));
 
         showMenu = true;
     }
